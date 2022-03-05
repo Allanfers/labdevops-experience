12a489bd486f2dec431f07f3818f8c78b22bdbb4
Allanfers <60447483+Allanfers@users.noreply.github.com>
2022-03-05 15:38:18 -0300

    Retornando à etapa Bug Added - tentativa 2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Allan Maia"
 
 @app.route('/bug')                                                                                                                                
 def bad():                                                                                                                                        
